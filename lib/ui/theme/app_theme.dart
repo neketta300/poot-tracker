cc22f29c90c768fe8a00b493df188bbd09a88ecb
neketta300 <neketta300@gmail.com>
2026-02-19 16:15:46 +0300

    refactor(theme): add fonts style

--- lib/ui/theme/app_theme.dart
+++ lib/ui/theme/app_theme.dart
@@ -1,18 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppTheme {
   static ThemeData get lightTheme {
     return ThemeData(
       useMaterial3: true,
       fontFamily: 'Nunito',
-      colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal, brightness: Brightness.light)
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: Colors.teal,
+        brightness: Brightness.light,
+      ),
+      textTheme: TextTheme(
+        headlineLarge: TextStyle(fontSize: 36.sp, fontWeight: FontWeight.bold),
+        headlineMedium: TextStyle(fontSize: 28.sp, fontWeight: FontWeight.bold),
+        bodyLarge: TextStyle(fontSize: 18.sp),
+        bodyMedium: TextStyle(fontSize: 16.sp),
+        labelLarge: TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w600),
+      ),
     );
   }
+
   static ThemeData get darkTheme {
     return ThemeData(
       useMaterial3: true,
       fontFamily: 'Nunito',
-      colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal, brightness: Brightness.dark),
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: Colors.teal,
+        brightness: Brightness.dark,
+      ),
+      textTheme: TextTheme(
+        headlineLarge: TextStyle(fontSize: 36.sp, fontWeight: FontWeight.bold),
+        headlineMedium: TextStyle(fontSize: 28.sp, fontWeight: FontWeight.bold),
+        bodyLarge: TextStyle(fontSize: 18.sp),
+        bodyMedium: TextStyle(fontSize: 16.sp),
+        labelLarge: TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w600),
+      ),
     );
   }
 }
