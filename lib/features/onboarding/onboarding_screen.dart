a11a76581a8dde31be158a4a04a019ea2429740a
neketta300 <neketta300@gmail.com>
2026-02-20 12:25:28 +0300

    refactor: litle refactring

--- lib/features/onboarding/onboarding_screen.dart
+++ lib/features/onboarding/onboarding_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import '../../router/router.dart';
-import '../consent/consent_screen.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
