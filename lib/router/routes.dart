08024e25506df4c52628fdc6023c1761b14ce862
neketta300 <neketta300@gmail.com>
2026-02-22 14:47:38 +0300

    feat(routing): add auth rout

--- lib/router/routes.dart
+++ lib/router/routes.dart
@@ -1,5 +1,6 @@
 class AppRoutes {
   static const String onboarding = '/onboarding';
   static const String consent = '/consent';
+  static const String auth = '/auth';
   static const String dashboard = '/dashboard';
 }
